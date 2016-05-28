283392765d93f62de974604d160df6ab025afcc6
Simon Sticko <simon.sticko@it.uu.se>
2016-05-28 16:39:03 +0200

    Fix bug should be pgup.

--- keyboard/ergodox_ez/keymaps/sticko/keymap.c
+++ keyboard/ergodox_ez/keymaps/sticko/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_PGDN,        KC_HOME,      KC_PGDN,KC_END, KC_LALT,
+        KC_PGUP,        KC_HOME,      KC_PGDN,KC_END, KC_LALT,
 		                                                      KC_LGUI,KC_HOME,
                                                                       KC_END,
                                                       KC_SPC, KC_DEL, LT(SYMB,KC_GRV),
